666c47dfefc5a6e10a1a77558d31ff4a5254fa44
lassepe <lasse.peters@mailbox.org>
2021-07-30 10:18:08 +0200

    Implement quadraticization interface

--- pyilqr/costs.py
+++ pyilqr/costs.py
@@ -1,5 +1,42 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 import numpy as np
+
+
+class AbstractCosts(ABC):
+    @abstractmethod
+    def state_hessian(self, x) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def state_gradient(self, u) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def input_hessian(self, x) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def input_gradient(self, u) -> np.ndarray:
+        pass
+
+    def _quadratisized(self, x, to_hessian, to_gradient) -> "QuadraticCostPrimitive":
+        H = to_hessian(x)
+        g = to_gradient(x)
+        # TODO: think about scaling
+        return QuadraticCostPrimitive(H, g)
+
+    def quadratisized_along_trajectory(self, x_op, u_op) -> "QuadraticCost":
+        state_cost = [
+            self._quadratisized(x, self.state_hessian, self.state_gradient)
+            for x in x_op
+        ]
+        input_cost = [
+            self._quadratisized(u, self.input_hessian, self.input_gradient)
+            for u in u_op
+        ]
+        return QuadraticCost(state_cost, input_cost)
+
 
 @dataclass
 class QuadraticCostPrimitive:
@@ -14,10 +51,23 @@ class QuadraticCostPrimitive:
     def __call__(self, x: np.ndarray):
         return 0.5 * x.T @ self.Q @ x + self.l.T @ x
 
+
 @dataclass
-class QuadraticCost:
+class QuadraticCost(AbstractCosts):
     state_cost: list[QuadraticCostPrimitive]
     input_cost: list[QuadraticCostPrimitive]
+
+    def state_hessian(self, x):
+        raise NotImplementedError
+
+    def state_gradient(self, x):
+        raise NotImplementedError
+
+    def input_hessian(self, x):
+        raise NotImplementedError
+
+    def input_gradient(self, x):
+        raise NotImplementedError
 
     def Q(self, k: int):
         return self.state_cost[k].Q
