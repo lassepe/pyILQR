Fix sign error in unicycle linearization

pyilqr/example_dyanmics.py
@@ -20,14 +20,26 @@ class UnicycleDynamics(AbstractSampledDynamics):
         px, py, phi, v = x
         # input layout:
         dphi, dv = u
-        return np.array([v * np.cos(phi), v * np.sin(phi), dphi, dv])
+        return np.array(
+            [
+                v * np.cos(phi),
+                v * np.sin(phi),
+                dphi,
+                dv,
+            ]
+        )
 
     def linearized_continuous(self, x: np.ndarray, u: np.ndarray):
         px, py, phi, v = x
         sPhi = np.sin(phi)
         cPhi = np.cos(phi)
         A = np.array(
-            [[0, 0, v * sPhi, cPhi], [0, 0, v * cPhi, sPhi], [0, 0, 0, 0], [0, 0, 0, 0]]
+            [
+                [0, 0, -v * sPhi, cPhi],
+                [0, 0, v * cPhi, sPhi],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ]
         )
         B = np.array([[0, 0], [0, 0], [1, 0], [0, 1]])
         return (A, B)