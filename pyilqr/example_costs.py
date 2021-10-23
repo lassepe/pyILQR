Fix soft-constraint hessian

pyilqr/example_costs.py
@@ -34,7 +34,11 @@ class SoftConstraintCost(AbstractCost):
         return self.Q @ ex
 
     def hessian(self, x: np.ndarray):
-        return self.Q
+        active_vector_mask = x < self.x_min | x > self.x_max
+        active_matrix_mask = np.outer(active_vector_mask, active_vector_mask)
+        Q = np.zeros_like(self.Q)
+        Q[active_matrix_mask] = self.Q[active_matrix_mask]
+        return Q
 
 
 @dataclass